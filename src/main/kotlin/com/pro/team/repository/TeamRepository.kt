e08e195eedd6e1ef1adb063f6f5f46cc1b7d3538
koreaaerospaceU <zx811@naver.com>
2022-02-08 01:28:42 +0900

    feat: Team Repository 메소드 정의 - TeamToken 조건 검색 메소드 정의 - TeamID 와 UUID 조건 검색 메소드 정의

--- src/main/kotlin/com/pro/team/repository/TeamRepository.kt
+++ src/main/kotlin/com/pro/team/repository/TeamRepository.kt
@@ -3,6 +3,7 @@ package com.pro.team.repository
 import com.pro.team.domain.Team
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 /**
  * Created by Minky on 2022-02-07
@@ -10,4 +11,6 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface TeamRepository: JpaRepository<Team, Long> {
+    fun findByIdAndUuid(id: Long, uuid: String): Optional<Team>
+    fun findByTeamToken(teamToken: String): Optional<Team>
 }
