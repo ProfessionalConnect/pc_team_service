8efa24c40c824398df8d648ed4ec9f2273b0c8da
koreaaerospaceU <zx811@naver.com>
2022-02-14 20:44:44 +0900

    feat: 응답 DTO 생성, 수정 시간 필드 추가

--- src/main/kotlin/com/pro/team/dto/TeamMemberResponse.kt
+++ src/main/kotlin/com/pro/team/dto/TeamMemberResponse.kt
@@ -1,6 +1,7 @@
 package com.pro.team.dto
 
 import com.pro.team.domain.TeamMember
+import java.time.LocalDateTime
 
 /**
  * Created by Minky on 2022-02-07
@@ -8,10 +9,17 @@ import com.pro.team.domain.TeamMember
 data class TeamMemberResponse(
     val uuid: String,
     val memberType: String,
+    val createdDate: LocalDateTime,
+    val lastModifiedDate: LocalDateTime,
 ) {
     companion object {
         fun of(teamMember: TeamMember) =
-            TeamMemberResponse(teamMember.uuid, teamMember.memberType.name)
+            TeamMemberResponse(
+                teamMember.uuid,
+                teamMember.memberType.name,
+                teamMember.createdDate,
+                teamMember.lastModifiedDate
+            )
 
         fun listOf(teamMemberList: List<TeamMember>) =
             teamMemberList.map(TeamMemberResponse::of)
--- src/main/kotlin/com/pro/team/dto/TeamResponse.kt
+++ src/main/kotlin/com/pro/team/dto/TeamResponse.kt
@@ -1,6 +1,7 @@
 package com.pro.team.dto
 
 import com.pro.team.domain.Team
+import java.time.LocalDateTime
 
 /**
  * Created by Minky on 2022-02-07
@@ -9,10 +10,18 @@ data class TeamResponse(
     val id: Long?,
     val teamName: String,
     val description: String,
+    val createdDate: LocalDateTime,
+    val lastModifiedDate: LocalDateTime,
 ) {
     companion object {
         fun of(team: Team) =
-            TeamResponse(team.id, team.teamName, team.description)
+            TeamResponse(
+                team.id,
+                team.teamName,
+                team.description,
+                team.createdDate,
+                team.lastModifiedDate
+            )
 
         fun listOf(teamList: List<Team>) =
             teamList.map(TeamResponse::of)
